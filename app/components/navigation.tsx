db6f6d1082e8c0bc66065a7fcae2a18a7177d197
heismyke <81974207+heismyke@users.noreply.github.com>
2025-04-07 15:53:12 +0100

    navbar revamped

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import { RiHome7Fill, RiMenuLine } from 'react-icons/ri'
+import { RiHome7Fill } from 'react-icons/ri'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
